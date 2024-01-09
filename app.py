d6c14221ea0ba59a2817a86ff9ac2e564e7d8167
Tejas Sharma <120678627+Tej-as1@users.noreply.github.com>
2024-01-10 00:11:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-!pip install joblib
 import streamlit as st
 from PIL import Image
 import os
 import pandas as pd
 import numpy as np
-import joblib
+import pickle
 
-model = joblib.load('RegressionModel.pkl')
+with open('RegressionModel.pkl', 'rb') as file:
+    model = pickle.load(file)
 
 st.title('Predict Number of Coins in an Image')
 
@@ -15,15 +15,9 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image', use_column_width=True)
-
+    
     if st.button('Predict'):
         img_array = np.array(image.convert('L').resize((28, 28)))
-
         img_flatten = img_array.flatten().reshape(1, -1)
-
         predicted_coins = model.predict(img_flatten)
-
         st.write(f"Predicted Number of Coins: {predicted_coins[0]}")
-
-
-
